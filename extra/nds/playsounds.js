cf18db6d4bd703cc56a7cf5dd28a1a31bea8764c
Cycloneblaze <me@cycloneblaze.net>
2021-05-10 02:37:01 +0200

    play nice sounds

--- extra/nds/playsounds.js
+++ extra/nds/playsounds.js
@@ -1,24 +1,24 @@
-const s_cancel = new Audio('cancel.wav');
-const s_close = new Audio('close.wav');
-const s_cursor = new Audio('cursor.wav');
-const s_keyup = new Audio('keyup.wav');
-const s_keydown = new Audio('keydown.wav');
-const s_ok = new Audio('ok.wav');
-const s_open = new Audio('open.wav');
-const s_select = new Audio('select.wav');
-const s_tick = new Audio('tick.wav');
-const s_tock = new Audio('tock.wav');
+const s_cancel = new Audio('/extra/nds/cancel.wav');
+const s_close = new Audio('/extra/nds/close.wav');
+const s_cursor = new Audio('/extra/nds/cursor.wav');
+const s_keyup = new Audio('/extra/nds/keyup.wav');
+const s_keydown = new Audio('/extra/nds/keydown.wav');
+const s_ok = new Audio('/extra/nds/ok.wav');
+const s_open = new Audio('/extra/nds/open.wav');
+const s_select = new Audio('/extra/nds/select.wav');
+const s_tick = new Audio('/extra/nds/tick.wav');
+const s_tock = new Audio('/extra/nds/tock.wav');
 
-function playSound(soundName) {
-  if(soundName === "select") {
-    s_select.play();
-  }
-}
-
-function registerSounds() {
-  let elem = document.getElementById("settings");
-  elem.onclick = playSound; // to ensure this is the caller
-  elem.addEventListener('click', function(){
-    playSound("select"); // 'this' is <div class="colorBox"> (the clicked element) in this context
+function registerSound(elem_id, sound) {
+  let elem = document.getElementById(elem_id);
+  elem.onclick = sound.play; // to ensure this is the caller
+  elem.addEventListener('click', function(event){
+    event.preventDefault();
+    const goTO = this.getAttribute("href"); 
+    sound.play();
+    setTimeout(function(){ window.location = goTO; }, (sound.duration * 1000));
   });
 }
+
+
+
